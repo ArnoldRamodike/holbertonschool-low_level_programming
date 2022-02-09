75b01334357132141277001336ed20274990419e
ArnoldRamodike <95076110+ArnoldRamodike@users.noreply.github.com>
2022-02-09 15:45:19 +0200

    Prints n Number of tables

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -9,47 +9,44 @@
 
 void print_times_table(int n)
 {
-	int a, b, c, d, e, f;
+        int x, y, z;
 
-	if (n >= 0 && n < 15)
-	{
-		for (a = 0; a <=  n; a++)
-		{
-			for (b = 0; b <= n; b++)
-			{
-				c = a * b; d = c / 100;
-				e = (c /  10) % 10;
-				f = (c % 100) % 10;
-				if (b == 0)
-				{
-					_putchar('0');
-				}
-				else if (c < 10)
-				{
-					_putchar(' ');
-					_putchar(' ');
-					_putchar('0' + f);
-				}
-				else if (c < 100)
-				{
-					_putchar(' ');
-					_putchar('0' + e);
-					_putchar('0' + f);
-				}
-				else
-				{
-					_putchar('0' + d);
-					_putchar('0' + e);
-					_putchar('0' + f);
-				}
-				if (b < n)
-				{
-					_putchar(',');
-					_putchar(' ');
-				}
-				else
-					_putchar('\n');
-			}
-		}
-	}
+        if (n >= 0 && n <= 14)
+        {
+                for (x = 0; x <= n; x++)
+                {
+                        for (y = 0; y <= n; y++)
+                        {
+                                z = x * y;
+                                if (z > 99)
+                                {
+                                        _putchar(',');
+                                        _putchar(32);
+                                        _putchar((z / 100) + '0');
+                                        _putchar(((z / 10) % 10) + '0');
+                                        _putchar((z % 10) + '0');
+                                }
+                                else if (z > 9)
+                                {
+                                        _putchar(',');
+                                        _putchar(32);
+                                        _putchar(32);
+                                        _putchar(((z / 10) % 10) + '0');
+                                        _putchar((z % 10) + '0');
+                                }
+                                else
+                                {
+                                        if (y != 0)
+                                        {
+                                                _putchar(',');
+                                                _putchar(32);
+                                                _putchar(32);
+                                                _putchar(32);
+                                        }
+                                        _putchar(z + '0');
+                                }
+                        }
+                        _putchar('\n');
+                }
+        }
 }
