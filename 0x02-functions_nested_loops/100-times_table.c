89db3d6ba87de42e8026bf7d4ea4443b16aad9f2
ArnoldRamodike <95076110+ArnoldRamodike@users.noreply.github.com>
2022-02-09 15:51:57 +0200

    Update 100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,54 +1,56 @@
 #include "main.h"
 
-/**
- * print_times_table - prints the times table up to the integer input
- * @n: integer input
- *
- * Return: void
- */
 
+
+/**
+* print_times_table - Prints the times table of the input,
+* starting with 0.
+* @n: The value of the times table to be printed.
+*/
 void print_times_table(int n)
 {
-	int a, b, c, d, e, f;
+int num, mult, prod;
 
-	if (n >= 0 && n < 15)
-	{
-		for (a = 0; a <=  n; a++)
-		{
-			for (b = 0; b <= n; b++)
-			{
-				c = a * b; d = c / 100; e = (c /  10) % 10;
-				f = (c % 100) % 10;
-				if (b == 0)
-				{
-					_putchar('0');
-				}
-				else if (c < 10)
-				{
-					_putchar(' ');
-					_putchar(' ');
-					_putchar('0' + f);
-				}
-				else if (c < 100)
-				{
-					_putchar(' ');
-					_putchar('0' + e);
-					_putchar('0' + f);
-				}
-				else
-				{
-					_putchar('0' + d);
-					_putchar('0' + e);
-					_putchar('0' + f);
-				}
-				if (b < n)
-				{
-					_putchar(',');
-					_putchar(' ');
-				}
-				else
-					_putchar('\n');
-			}
-		}
-	}
+
+
+if (n >= 0 && n <= 15)
+{
+for (num = 0; num <= n; num++)
+{
+_putchar('0');
+
+
+
+for (mult = 1; mult <= n; mult++)
+{
+_putchar(',');
+_putchar(' ');
+
+
+
+prod = num * mult;
+
+
+
+if (prod <= 99)
+_putchar(' ');
+if (prod <= 9)
+_putchar(' ');
+
+
+
+if (prod >= 100)
+{
+_putchar((prod / 100) + '0');
+_putchar(((prod / 10)) % 10 + '0');
+}
+else if (prod <= 99 && prod >= 10)
+{
+_putchar((prod / 10) + '0');
+}
+_putchar((prod % 10) + '0');
+}
+_putchar('\n');
+}
+}
 }
