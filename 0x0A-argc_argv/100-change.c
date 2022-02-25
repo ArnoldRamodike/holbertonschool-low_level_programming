2d4cb8b2654393a18d5dfe46ad23910857a829cd
ArnoldRamodike <95076110+ArnoldRamodike@users.noreply.github.com>
2022-02-25 13:39:19 +0200

    Update 100-change.c

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,54 +1,48 @@
-#include "main.h"
-#include <ctype.h>
+#include <stdio.h>
 #include <stdlib.h>
 
 /**
- * main - Find the smallest number of coins to make an amount of change
- * @argc: Arg count
- * @argv: Contains all arguments
- *
- * Return: 1 on error, 0 for success
+ * main - function to print out the change that needs to given
+ * @argc: number of arguments to be passed
+ * @argv: number of variables to calculate
+ * Return: always 0 for sucess
  */
-int main(int argc, char *argv[])
-{	int cents, coins = 0;
+int main(int argc, char **argv)
+{
+    int total, change;
+    if (argc < 2)
+    {
+        printf("Error\n");
+        return (1);
+    }
 
-	if (argc == 2)
-	{
-		cents = atoi(*(argv + 1));
-		while (cents > 0)
-		{
-			if (cents % 25 < cents)
-			{
-				cents -= 25;
-				coins++;
-			}
-			else if (cents % 10 < cents)
-			{
-				cents -= 10;
-				coins++;
-			}
-			else if (cents % 5 < cents)
-			{
-				cents -= 5;
-				coins++;
-			}
-			else if (cents % 2 < cents)
-			{
-				cents -= 2;
-				coins++;
-			}
-			else if (cents % 1 < cents)
-			{
-				cents -= 1;
-				coins++;
-			}
-		}
-	}
-	else
-	{
-		printf("Error\n");
-		return (1);
-	}
-	printf("%d\n", coins);
-	return (0);
+    change = atoi(argv[1]);
+
+    for (total = 0; change > 0; total++)
+    {
+        if (change - 25 >= 0)
+           change = change - 25;
+
+        else if (change - 10 >= 0)
+
+            change = change - 10;
+
+        else if (change - 5 >= 0)
+
+            change = change - 5;
+
+        else if (change - 2 >= 0)
+
+            change = change - 2;
+
+        else if (change - 1 >= 0)
+
+            change = change - 1;
+
+    }
+
+    printf("%d\n", total);
+
+    return (0);
+
 }
