65da4a279969fea3314a253cd07cfeec59a6b13f
ArnoldRamodike <95076110+ArnoldRamodike@users.noreply.github.com>
2022-03-22 14:36:57 +0200

    Add Function

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,65 +1,53 @@
-#include "lists.h"
 #include <stdlib.h>
-#include <stdio.h>
+#include "lists.h"
 
 /**
- * _ra - reallocates memory for an array of pointers
- * to the nodes in a linked list
- * @list: the old list to append
- * @size: size of the new list (always one more than the old list)
- * @new: new node to add to the list
- *
- * Return: pointer to the new list
+ * free_listint_safe - Free a list that may or may not loop,
+ * set start of list to NULL
+ * @h: Pointer to pointer to the start of the list
+ * Return: Size of the list that has been freed
  */
-listint_t **_ra(listint_t **list, size_t size, listint_t *new)
+size_t free_listint_safe(listint_t **h)
 {
-	listint_t **newlist;
-	size_t i;
+	listint_t *killnode;
+	listint_t *current;
+	listadd_t *headadd;
+	listadd_t *checker;
+	size_t count;
 
-	newlist = malloc(size * sizeof(listint_t *));
-	if (newlist == NULL)
+	count = 0;
+	current = *h;
+	headadd = NULL;
+	if (h != NULL)
 	{
-		free(list);
-		exit(98);
-	}
-	for (i = 0; i < size - 1; i++)
-		newlist[i] = list[i];
-	newlist[i] = new;
-	free(list);
-	return (newlist);
-}
-
-/**
- * free_listint_safe - frees a listint_t linked list.
- * @head: double pointer to the start of the list
- *
- * Return: the number of nodes in the list
- */
-size_t free_listint_safe(listint_t **head)
-{
-	size_t i, num = 0;
-	listint_t **list = NULL;
-	listint_t *next;
-
-	if (head == NULL || *head == NULL)
-		return (num);
-	while (*head != NULL)
-	{
-		for (i = 0; i < num; i++)
+		while (current != NULL)
 		{
-			if (*head == list[i])
+			checker = headadd;
+			while (checker != NULL)
 			{
-				*head = NULL;
-				free(list);
-				return (num);
+				if (current == checker->address)
+				{
+					free(current);
+					free_listadd(headadd);
+					/*headadd = NULL;*/
+					 *h = NULL;
+					return (count);
+				}
+				checker = checker->next;
 			}
+			killnode = current;
+			if (add_nodeaddress(&headadd, current) == NULL)
+			{
+				free_listadd(headadd);
+				exit(98);
+			}
+			current = current->next;
+			free(killnode);
+			count++;
 		}
-		num++;
-		list = _ra(list, num, *head);
-		next = (*head)->next;
-		free(*head);
-		*head = next;
+		free_listadd(headadd);
+		/*headadd = NULL;*/
+		*h = NULL;
 	}
-	free(list);
-	return (num);
+	return (count);
 }
